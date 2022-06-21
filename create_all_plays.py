647b30a5fbc9a60c88d58d864aa0945dd54d598e
Reinaldo Neto <reinaldonetof@hotmail.com>
2022-06-21 09:56:51 -0300

    fix variables

--- create_all_plays.py
+++ create_all_plays.py
@@ -3,44 +3,44 @@ import itertools
 import pickle
 
 
-def score_guess(palpite, resposta):
+def resultado_do_palpite(palpite, resposta):
     # Iteração item a item para determinar o resultado entre o palpite e a resposta
-    hints = []
-    ans_no_match = []
-    guess_no_match = []
+    dicas = []
+    resposta_sem_equivalencia = []
+    palpite_em_equivalencia = []
     # Quebra a string em pedaços, Ex: RRRR, GBYG -> ["R", "R", "R", "R"], ["G", "B", "Y", "G"]
     # fazendo a iteração entre cada um dos itens
-    for guess_elem, ans_elem in zip(palpite, resposta):
-        if guess_elem == ans_elem:
-            hints.append("B")
+    for palpite_elem, resposta_elem in zip(palpite, resposta):
+        if palpite_elem == resposta_elem:
+            dicas.append("B")
         else:
-            guess_no_match.append(guess_elem)
-            ans_no_match.append(ans_elem)
+            palpite_em_equivalencia.append(palpite_elem)
+            resposta_sem_equivalencia.append(resposta_elem)
 
-    for guess_elem in guess_no_match:
-        if guess_elem in ans_no_match:
-            hints.append("W")
-            ans_no_match.remove(guess_elem)
+    for palpite_elem in palpite_em_equivalencia:
+        if palpite_elem in resposta_sem_equivalencia:
+            dicas.append("W")
+            resposta_sem_equivalencia.remove(palpite_elem)
 
-    return "".join(hints)
+    return "".join(dicas)
 
 
 def build_dict():
     # Cria o estrutura do tipo dicionário
-    score_dict = collections.defaultdict(dict)
+    dicionario_de_resultados = collections.defaultdict(dict)
     # Faz a iteração entre todas as variáveis, Ex: RRRR, RRRG, ..., RGBY, ..., OOOP, OOOO
-    all_guesses = itertools.product(["R", "G", "B", "Y", "P", "O"], repeat=4)
+    lista_de_resultados = itertools.product(["R", "G", "B", "Y", "P", "O"], repeat=4)
 
-    for guess, answer in itertools.product(all_guesses, repeat=2):
-        guess_str = "".join(guess)
-        ans_str = "".join(answer)
+    for palpite, reposta in itertools.product(lista_de_resultados, repeat=2):
+        palpite_str = "".join(palpite)
+        reposta_str = "".join(reposta)
         # Armazena os dados como um objeto,
         # Ex: RRRR: { RRRR: "BBBB", RRRG: "BBB", ..., OOOO: ""}, ..., RGBY: { RRRR: "B", RRRG: "BW", ..., OOOO: ""}
-        score_dict[guess_str][ans_str] = score_guess(guess, answer)
+        dicionario_de_resultados[palpite_str][reposta_str] = resultado_do_palpite(palpite, reposta)
 
     # Salva o Dicionário em um arquivo para evitar o consumo de tempo e memória todas as vezes em que iniciar o jogo
     FileStore = open("storage/dicionario_resultados.pickle", "wb")
-    pickle.dump(score_dict, FileStore)
+    pickle.dump(dicionario_de_resultados, FileStore)
     FileStore.close()
 
 
--- mastermind_solver.py
+++ mastermind_solver.py
@@ -1,32 +1,32 @@
 import pickle
 import itertools
 import collections
-from create_all_plays import score_guess
+from create_all_plays import resultado_do_palpite
 
 
 class MastermindCodeBreaker:
-    def __init__(self, answer):
+    def __init__(self, senha_cm):
         # Cria a lista de resultados LR
-        self.all_answers = itertools.product(["R", "G", "B", "Y", "P", "O"], repeat=4)
+        self.lista_de_resultados = itertools.product(["R", "G", "B", "Y", "P", "O"], repeat=4)
         # A LR é uma lista de arrays, aqui o JOIN transforma em string
-        self.all_answers = {"".join(answer) for answer in self.all_answers}
+        self.lista_de_resultados = {"".join(answer) for answer in self.lista_de_resultados}
         # Quantidade padrão para o jogo Mastermind, mesmo sabendo que não será necessário toda essa quantidade
-        self.palpites_left = 10
-        self.palpite_history = []
-        self.hint_history = []
-        self.answer = answer
+        self.palpites_restantes = 10
+        self.historico_palpites = []
+        self.historico_de_dicas = []
+        self.senha_cm = senha_cm
 
         FileStore = open("storage/dicionario_resultados.pickle", "rb")
-        self.score_dict = pickle.load(FileStore)
+        self.dicionaria_de_resultados = pickle.load(FileStore)
         FileStore.close()
 
-    def make_palpite(self):
-        palpites_to_try = []
-        for palpite, scores_by_answer_dict in self.score_dict.items():
+    def criar_palpite(self):
+        palpites_para_tentativas = []
+        for palpite, scores_by_answer_dict in self.dicionaria_de_resultados.items():
             # Filtra para incluir apenas as respostas possíveis
             scores_by_answer_dict = {answer: score for answer, score in scores_by_answer_dict.items()
-                                     if answer in self.all_answers}
-            self.score_dict[palpite] = scores_by_answer_dict
+                                     if answer in self.lista_de_resultados}
+            self.dicionaria_de_resultados[palpite] = scores_by_answer_dict
 
             # Faz a contagem de cada um dos valores obtidos, Ex: {"BBB": 10, "B": 7, "BW": 20}
             possibilities_per_score = collections.Counter(scores_by_answer_dict.values())
@@ -34,33 +34,33 @@ class MastermindCodeBreaker:
             pior_caso = max(possibilities_per_score.values())
 
             # É preferível palpites possíveis em vez dos impossíveis
-            palpite_impossivel = palpite not in self.all_answers
-            palpites_to_try.append((pior_caso, palpite_impossivel, palpite))
+            palpite_impossivel = palpite not in self.lista_de_resultados
+            palpites_para_tentativas.append((pior_caso, palpite_impossivel, palpite))
 
         # Quem vai determinar qual o próximo palpite é o minimo, temos um array de array
         # Ex: [(72, True, 'RRRR'), (64, True, 'RRRG'), (42, True, 'RRRB'),...]
         # Se o primeiro valor for igual, o segundo é quem vai desempatar,
         # nesse caso a função dá prioridade para o FALSE, ou seja, para valores possíveis.
         # Mas terão momentos em que serão utilizados palpites impossíveis.
-        test = min(palpites_to_try)
+        test = min(palpites_para_tentativas)
         test2 = test[-1]
         return test2
 
     def solve(self):
-        while self.palpites_left > 0:
-            if self.palpites_left == 10:
+        while self.palpites_restantes > 0:
+            if self.palpites_restantes == 10:
                 palpite = "RRGG"
             else:
-                palpite = self.make_palpite()
+                palpite = self.criar_palpite()
 
-            self.palpite_history.append(palpite)
+            self.historico_palpites.append(palpite)
 
-            score = score_guess(palpite, self.answer)
-            self.hint_history.append(score)
+            resultado = resultado_do_palpite(palpite, self.senha_cm)
+            self.historico_de_dicas.append(resultado)
             # Filtro em busca apenas dentro da chave do palpite pelos valores iguais ao resultado
-            self.all_answers = {answer for answer in self.all_answers
-                                if self.score_dict[palpite][answer] == score}
+            self.lista_de_resultados = {answer for answer in self.lista_de_resultados
+                                if self.dicionaria_de_resultados[palpite][answer] == resultado}
 
-            self.palpites_left -= 1
-            if palpite == self.answer:
-                return self.palpite_history, self.hint_history
+            self.palpites_restantes -= 1
+            if palpite == self.senha_cm:
+                return self.historico_palpites, self.historico_de_dicas
